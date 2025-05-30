17d5bd6273b54dbf06656aa3ef814b13864abd6e
atomisadev <atomisadev@pm.me>
2025-05-30 04:28:40 +0000

    project setup and cors

---
+++ apps/api/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var myAllowAllOriginsPolicy = "_myAllowAllOriginsPolicy";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowAllOriginsPolicy, policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(myAllowAllOriginsPolicy);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
