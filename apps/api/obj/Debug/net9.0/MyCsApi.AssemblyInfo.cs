e7d0f37f54c087d06a51192cffcab2c65d10bfa5
atomisadev <atomisadev@pm.me>
2025-05-31 17:29:42 +0000

    working

--- apps/api/obj/Debug/net9.0/MyCsApi.AssemblyInfo.cs
+++ apps/api/obj/Debug/net9.0/MyCsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51327e86152cd2c3a586b5200d2243ddf574e6b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a79b6d8f72bfa8e85fea62dadcab95500554d80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
