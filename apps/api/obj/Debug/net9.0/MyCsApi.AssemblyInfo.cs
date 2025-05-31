54a49c2acae8fed92ac58d810feef0c4178e008b
atomisadev <atomisadev@pm.me>
2025-05-31 13:59:37 +0000

    removing scripts

--- apps/api/obj/Debug/net9.0/MyCsApi.AssemblyInfo.cs
+++ apps/api/obj/Debug/net9.0/MyCsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea000bf3ef41cf7ce4b9e95e72761ab7a7f7ee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ef378eaf20970bc1c3da9f03bb76a6ca52479f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
